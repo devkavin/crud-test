d1fbf64e52cf61106981187e725f741fa9471858
devkavin <kavindra.senanayake@gmail.com>
2024-11-08 02:35:05 +0530

    Update temporary user details in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => env('TEMP_USER_NAME'),
+            'email' => env('TEMP_USER_EMAIL'),
+            'password' => bcrypt(env('TEMP_USER_PASSWORD')),
         ]);
     }
 }
